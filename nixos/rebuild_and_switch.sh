458fc32e9cd4f2fdba87b8d8b3538527d3592ccf
Matthew O'Gorman <mog@rldn.net>
2019-04-10 18:52:28 -0400

    better versioning

--- nixos/rebuild_and_switch.sh
+++ nixos/rebuild_and_switch.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-export NIXOS_LABEL=$(nix-info -m|grep channels\(root|cut -f2 -d'"'|cut -f2 -d'-')
-export NIXOS_LABEL_VERSION=$(git rev-parse --short HEAD)
+export NIXOS_LABEL="$(nix-info -m|grep channels\(root|cut -f2 -d'"'|cut -f2 -d'-')_$(git rev-parse --short HEAD)"
 nixos-rebuild switch
 
