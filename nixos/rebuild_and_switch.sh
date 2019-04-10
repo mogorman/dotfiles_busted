1b357f7338baba509ba07b67fed23ce37498251c
Matthew O'Gorman <mog@rldn.net>
2019-04-10 03:56:15 -0400

    doing labeling correctly

--- nixos/rebuild_and_switch.sh
+++ nixos/rebuild_and_switch.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
-export NIXOS_LABEL_VERSION=$(git rev-parse --short HEAD); nixos-rebuild switch
+export NIXOS_LABEL=$(nix-info -m|grep channels\(root|cut -f2 -d'"'|cut -f2 -d'-')
+export NIXOS_LABEL_VERSION=$(git rev-parse --short HEAD)
+nixos-rebuild switch
 
