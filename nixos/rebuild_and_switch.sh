06e52142a21174fdbb4785eceea451f8c5f23294
Matthew O'Gorman <mog@rldn.net>
2019-04-10 20:04:30 -0400

    make it so im less likely to rebuild without commiting changes

--- nixos/rebuild_and_switch.sh
+++ nixos/rebuild_and_switch.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
-export NIXOS_LABEL="$(nix-info -m|grep channels\(root|cut -f2 -d'"'|cut -f2 -d'-')_$(git rev-parse --short HEAD)"
-nixos-rebuild switch
-
+UNSAVED_CHANGES=$(git diff-index --name-only HEAD --)
+if [ -n "${UNSAVED_CHANGES}" ]; then
+	echo "commit your changes first mog"
+else
+	export NIXOS_LABEL="$(nix-info -m|grep channels\(root|cut -f2 -d'"'|cut -f2 -d'-')_$(git rev-parse --short HEAD)"
+	nixos-rebuild switch
+fi
