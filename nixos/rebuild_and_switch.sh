1265f7a23514bdbd94b7fe4d33b230451e21cfe4
Matthew O'Gorman <mog@rldn.net>
2019-04-12 21:45:47 -0400

    to -> too

--- nixos/rebuild_and_switch.sh
+++ nixos/rebuild_and_switch.sh
@@ -3,7 +3,7 @@ UNSAVED_CHANGES=$(git diff-index --name-only HEAD --)
 if [ -n "${UNSAVED_CHANGES}" ]; then
 	echo "commit your changes first mog"
 elif [ $(git rev-parse HEAD) != $(git rev-parse @{u}) ]; then
-	echo "push your changes to mog!"
+	echo "push your changes too mog!"
 else
 	export NIXOS_LABEL="$(nix-info -m|grep channels\(root|cut -f2 -d'"'|cut -f2 -d'-')_$(git rev-parse --short HEAD)"
 	nixos-rebuild switch
