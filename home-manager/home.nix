ee2213c4942baeaa322064f66ec8921b3c4676a2
Matthew O'Gorman <mog@rldn.net>
2019-04-18 05:20:44 -0400

    dunst to home-manager

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -1,4 +1,13 @@
 { pkgs, ... }:
 {
   home.file.".stumpwm.d".source = "/home/mog/code/dotfiles/home-manager/stumpwm";
+  services.dunst.enable = true;
+  services.dunst.settings = {
+  global = {
+    geometry = "400x5-30+50";
+    transparency = 10;
+    frame_color = "#eceff1";
+    font = "Terminus 10";
+  };
+  };
 }
--- home-manager/stumpwm/init.lisp
+++ home-manager/stumpwm/init.lisp
@@ -19,7 +19,7 @@
 
 (set-module-dir "~/.stumpwm.d/modules")
 
-(toggle-mode-line (current-screen) (current-head))
+(enable-mode-line (current-screen) (current-head))
 
 (load-module "stumptray")
 (stumptray:stumptray)
@@ -48,8 +48,8 @@
   (run-or-raise "firefox" '(:instance "firefox")))
 
 (defcommand chrome () ()
-  "Start google-chome-stable unless it is already running, in which case focus it."
-  (run-or-raise "google-chome-stable" '(:instance "google-chrome-stable")))
+  "Start google-chrome-stable unless it is already running, in which case focus it."
+  (run-or-raise "google-chrome-stable" '(:instance "google-chrome-stable")))
 
 (defgeneric modeline-window-list (group))
 
@@ -134,36 +134,6 @@
 
 ;; Clear rules
 (clear-window-placement-rules)
-
-;; Last rule to match takes precedence!
-;; TIP: if the argument to :title or :role begins with an ellipsis, a substring
-;; match is performed.
-;; TIP: if the :create flag is set then a missing group will be created and
-;; restored from *data-dir*/create file.
-;; TIP: if the :restore flag is set then group dump is restored even for an
-;; existing group using *data-dir*/restore file.
-(define-frame-preference "Default"
-  ;; frame raise lock (lock AND raise == jumpto)
-  (0 t nil :class "Konqueror" :role "...konqueror-mainwindow")
-  (1 t nil :class "XTerm"))
-
-(define-frame-preference "Ardour"
-  (0 t   t   :instance "ardour_editor" :type :normal)
-  (0 t   t   :title "Ardour - Session Control")
-  (0 nil nil :class "XTerm")
-  (1 t   nil :type :normal)
-  (1 t   t   :instance "ardour_mixer")
-  (2 t   t   :instance "jvmetro")
-  (1 t   t   :instance "qjackctl")
-  (3 t   t   :instance "qjackctl" :role "qjackctlMainForm"))
-
-(define-frame-preference "Shareland"
-  (0 t   nil :class "XTerm")
-  (1 nil t   :class "aMule"))
-
-(define-frame-preference "Emacs"
-  (1 t t :restore "emacs-editing-dump" :title "...xdvi")
-  (0 t t :create "emacs-dump" :class "Emacs"))
 
 (define-key *root-map* (kbd "~") "exec")
 (define-key *root-map* (kbd "C-r") "exec")
